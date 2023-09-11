6bb971ac2fdd9b3180548f90e4fc0eb3946f00a7
Fellow-DevJk <117641012+Fellow-DevJk@users.noreply.github.com>
2023-09-11 21:53:33 +0530

    C program for SRTF cpu scheduling algorithm

--- C Programs/SRTF_CPUscheduling.c
+++ C Programs/SRTF_CPUscheduling.c
@@ -34,33 +34,39 @@ int main()
     printf("Enter your id, arrival time and burst: ");
     for (int i = 0; i < input; i++)
     {
-        scanf("%d %d %d", &cpu[i].id, &cpu[i].arrival, &cpu[i].burst); // taking the input process id, the arrival and burst time from the user
+        scanf("%d %d %d", &cpu[i].id, &cpu[i].arrival, &cpu[i].burst); // taking the input process id, the arrival and burst time from the use
         total_time += cpu[i].burst;
     }
     Bubble_Sort(cpu, input);
+    int *remaining = malloc(input * sizeof(int)); // creating an array for containing the remaining times after the process is preempted or over
     int *completion = calloc(input, sizeof(int)); // using calloc to initialise all the values at address to be 0
     int turnaround[input], waiting[input];        // creating arrays for containing the turnaround and waiting time of all the processes
+    for (int i = 0; i < input; i++)
+    {
+        remaining[i] = cpu[i].burst;
+    }
     int current_time = 0;
-    int empty = 0; // No. of processes that have been completed
-    float avg_turnaround = 0.0, avg_waiting = 0.0;
-
-    while (empty < input) // running till all processes are completed
+    int empty = 0;         // No. of processes that have been completed
+    while (empty != input) // running till all processes are completed
     {
         int min_index = -1, minimum = total_time;
-
         for (int i = 0; i < input; i++)
         {
-            if (cpu[i].arrival <= current_time && completion[i] <= 0 && cpu[i].burst < minimum) // checking for arrival times then finding the shortest burst time
+            if (cpu[i].arrival <= current_time && remaining[i] > 0 && remaining[i] < minimum) // checking for arrival times then finding the shortest remaining time
             {
+                minimum = remaining[i];
                 min_index = i;
-                minimum = cpu[i].burst;
             }
         }
         if (min_index != -1)
         {
-            completion[min_index] = current_time + cpu[min_index].burst; // running the process till it gets completed
-            current_time = completion[min_index];                        // current time will change to the time a process was completed
-            empty++;                                                     // incrementing the no.of processes completed
+            remaining[min_index] -= 1; // running the process till it gets preempted over
+            current_time++;
+            completion[min_index] = current_time;
+            if (remaining[min_index] == 0)
+            {
+                empty++; // incrementing the no.of processes completed
+            }
         }
         else
         {
@@ -68,7 +74,7 @@ int main()
         }
     }
     printf("Process ID\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time\n");
-
+    float avg_turnaround = 0.0, avg_waiting = 0.0;
     for (int i = 0; i < input; i++)
     { // Calculating the turnaround and waiting times and simultaneously printing it
         turnaround[i] = completion[i] - cpu[i].arrival;
@@ -81,8 +87,8 @@ int main()
     avg_turnaround /= input;
     avg_waiting /= input;
     printf("Average Waiting Time: %.2f\nAverage Turnaround Time: %.2f\n", avg_waiting, avg_turnaround);
-
     // freeing the dynamically allocated memory to avoid memory leaks
+    free(remaining);
     free(completion);
     free(cpu);
 
